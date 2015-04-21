ebe9b020fefd0ed3b7b588c5ee0229cda7d6cbc4
Craig Livingston <craiglivingston@knights.ucf.edu>
2015-04-21 15:36:21 -0400

    Done basically. Added proc -> symbol table Added lexLevel global and used where appropriate Added gen for CAL Tested for both test cases on webcourses (Fact and Nested) Worked for both cases, including modifying n in Fact
    Signed-off-by: Craig Livingston <craiglivingston@knights.ucf.edu>

--- files.h
+++ files.h
@@ -1,7 +1,7 @@
 #ifndef FILES_H
 #define FILES_H
 
-#define INPUT_FILE "inputGrade2.txt"
+#define INPUT_FILE "inputHW4b.txt"
 #define CLEAN_FILE "cleaninput.txt"
 #define TABLE_FILE "lexemetable.txt"
 #define LIST_FILE  "lexemelist.txt"
